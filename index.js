a2f4b0a931260b24204b74719cc9c0e2bd5c74b2
Thomas Mayrhofer <thomas@mayrhofer.at>
2022-10-31 12:40:05 +0100

    pass function as reference

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-const fetchAsync = (url) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log('executing', url)
-            resolve({data: url});
-            console.log('execution done', url)
-        }, Math.random() * 1000)
-    });
+const myFunction = function () {
+    return "hallo";
 }
 
-function a() {}
-function b() {}
-function c() {}
-
-async function fetchBestFriendsAddress() {
-    try {
-        const [user, weather] = await Promise.all([
-            fetchAsync('/api/current-user'),
-            fetchAsync('/api/weather')
-        ])
-
-        const bestFriend = await fetchAsync(`/api/users/${user.id}/best-friend`)
-        const address = await fetchAsync(`/api/users/${bestFriend.id}/address`)
-
-        console.log(address)
-    } catch(e) {
-        console.error(e)
-    }
-}
-
-async function main() {
-    a()
-    b()
-    await fetchBestFriendsAddress()
-    c()
-}
+console.log(myFunction())
 
 
+const callAndLogResult = (fn) => console.log(fn())
+
+callAndLogResult(myFunction)
